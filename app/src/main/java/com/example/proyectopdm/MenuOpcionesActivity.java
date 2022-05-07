87468dd08e8e1274cdb1a138f0ca179f189b5678
Luis318 <73361007+Luis318@users.noreply.github.com>
2022-05-06 19:14:41 -0600

    Mas cambios

--- app/src/main/java/com/example/proyectopdm/MenuOpcionesActivity.java
+++ app/src/main/java/com/example/proyectopdm/MenuOpcionesActivity.java
@@ -41,11 +41,9 @@ public class MenuOpcionesActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-<<<<<<< HEAD
-                R.id.nav_inicio, R.id.nav_miperfil, R.id.nav_proyectos, R.id.recordAcademicoFragment)
-=======
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_proyectos, R.id.bitacoraFragment, R.id.nav_estudiantes)
->>>>>>> luis123
+
+                R.id.nav_inicio, R.id.nav_miperfil, R.id.nav_proyectos, R.id.bitacoraFragment, R.id.nav_estudiantes,R.id.recordAcademicoFragment)
+
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_menu_opciones);
